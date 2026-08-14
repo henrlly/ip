87f37d41bdb8d11ff4bc0c752b008d408224bf53
Henry <80515759+henrlly@users.noreply.github.com>
2026-08-14 13:59:07 +0800

    Rename from duke to Bott

---
+++ src/main/java/Bott.java
@@ -0,0 +1,16 @@
+/**
+ * The main entry point for the Bott chatbot.
+ */
+public class Bott {
+
+    public static void main(String[] args) {
+        String banner =
+            " ____     ___     _____   _____ \n" +
+            "|  _ \\   / _ \\   |_   _| |_   _|\n" +
+            "| |_) | | | | |    | |     | |  \n" +
+            "|  _ <  | | | |    | |     | |  \n" +
+            "| |_) | | |_| |    | |     | |  \n" +
+            "|____/   \\___/     |_|     |_|  \n";
+        System.out.println(banner);
+    }
+}
